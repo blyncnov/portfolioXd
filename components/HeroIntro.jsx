0ea09d7879e7925a1f1423e504293cfa9b5f9de8
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-30 20:51:38 +0100

    finals

--- components/HeroIntro.jsx
+++ components/HeroIntro.jsx
@@ -1,62 +1,80 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 // Components
-import Button from "../constants/Button"
+import Button from "../constants/Button";
 
 // Data
 // import { HeroData } from "../data/HeroData"
 
-import Banner from "../public/Images/3d/biz.png"
+import Banner from "../public/Images/3d/biz.png";
 
 // styled-components
-import {
-    HeroContainerSection
-} from "../styles/styled/HeroPage"
+import { HeroContainerSection } from "../styles/styled/HeroPage";
 
 import {
-    Constraints, Section,
-    SectionInner, Divider,
-    HeadingOne, Paragraph,
-    FlexBoxToRow
-} from "../styles/constants/Constants"
-
+  Constraints,
+  Section,
+  SectionInner,
+  Divider,
+  HeadingOne,
+  Paragraph,
+  FlexBoxToRow,
+} from "../styles/constants/Constants";
 
 const HeroIntro = () => {
-    return (
-        <>
-            <HeroContainerSection >
-                <Constraints max="90%">
-                    <Section>
-                        <SectionInner>
-                            <Divider>
-                                <HeadingOne color="#ccd6f6">
-                                    <Paragraph color="#F9B590">Hi 👋 , my name is Taiwo Boluwatife</Paragraph> I Build Cool Web Related Stuff.
-                                </HeadingOne>
-                                <br />
-                                <Paragraph color="#8892B0"> I&apos;m a <span>Frontend</span> Major Developer specializing in building cool UI and great <span>User Experience </span> interfaces.
-                                    Currently i&apos;m focusing on transitioning into <span>Blockchain Development.</span>
-                                </Paragraph>
+  return (
+    <>
+      <HeroContainerSection>
+        <Constraints max="90%">
+          <Section>
+            <SectionInner>
+              <Divider>
+                <HeadingOne color="#ccd6f6">
+                  <Paragraph color="#F9B590">
+                    Hi 👋 , my name is Taiwo Boluwatife
+                  </Paragraph>{" "}
+                  I Build Cool Web Related Stuff.
+                </HeadingOne>
+                <br />
+                <Paragraph color="#8892B0">
+                  {" "}
+                  I&apos;m a <span>Frontend</span> Developer specializing in
+                  building cool UIs and great <span>User Experience </span>{" "}
+                  interfaces. i also have interest in{" "}
+                  <span>Blockchain Development.</span>
+                </Paragraph>
 
-                                <br />
-                                <FlexBoxToRow>
-                                    <Button tittle="Hire Me" urlData="mailto:bholuwatife00@gmail.com" />
-                                    <Button tittle="Check out my GitHub" urlData="https://github.com/blyncnov" />
-                                </FlexBoxToRow>
-                                <br />
-                            </Divider>
+                <br />
+                <FlexBoxToRow>
+                  <Button
+                    tittle="Hire Me"
+                    urlData="mailto:bholuwatife00@gmail.com"
+                  />
+                  <Button
+                    tittle="Check out my GitHub"
+                    urlData="https://github.com/blyncnov"
+                  />
+                </FlexBoxToRow>
+                <br />
+              </Divider>
 
-                            <Divider order="1">
-                                <Image style={{ borderRadius: "3px" }} src={Banner} placeholder="blur" blurDataURL={Banner} alt="banner" layout="responsive" />
-                            </Divider>
+              <Divider order="1">
+                <Image
+                  style={{ borderRadius: "3px" }}
+                  src={Banner}
+                  placeholder="blur"
+                  blurDataURL={Banner}
+                  alt="banner"
+                  layout="responsive"
+                />
+              </Divider>
+            </SectionInner>
+          </Section>
+        </Constraints>
+      </HeroContainerSection>
+    </>
+  );
+};
 
-                        </SectionInner>
-                    </Section>
-                </Constraints>
-            </HeroContainerSection>
-
-        </>
-    )
-}
-
-export default HeroIntro
+export default HeroIntro;
