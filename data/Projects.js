1e29a0a8ce2b8bb4bc2faae02d70fdbd8be3d0bf
Taiwo Boluwatife <blyncnov@Taiwos-MBP.lan>
2022-08-20 17:39:53 +0100

    fixed broken links

--- data/Projects.js
+++ data/Projects.js
@@ -1,99 +1,99 @@
-import Project from "../public/Images/assests/nextfood.png"
-import Project1 from "../public/Images/assests/ishortner.png"
-import Project2 from "../public/Images/assests/wpost.png"
-import Project3 from "../public/Images/assests/xdome.png"
-import Project4 from "../public/Images/assests/vitriol.png"
-import Project5 from "../public/Images/assests/fichta.png"
-
+import Project from "../public/Images/assests/nextfood.png";
+import Project1 from "../public/Images/assests/ishortner.png";
+import Project2 from "../public/Images/assests/wpost.png";
+import Project3 from "../public/Images/assests/xdome.png";
+import Project4 from "../public/Images/assests/vitriol.png";
+import Project5 from "../public/Images/assests/fichta.png";
+import Drophq from "../public/Images/assests/drophq.png";
+import Credizest from "../public/Images/assests/credizest.png";
 
 export const AllProjectData = [
-        {
-        id: 1,
-        ProjectName: "!Vitriol  ",
-        ProjectTechUsed: [
-            "Next Js",
-            "styled-Components",
-            "Redux toolkit",
-            "vercel",
-            "ethers js",
-            "MetaMask"
-        ],
-        ProjectDescription: "A web3 charity dapp Website template. Awesome UI , connected to ethers and blockchain.",
-            ProjectImage: Project4,
-          ProjectUrl: "https://vitriol-delta.vercel.app/",
-        order: 0,
-    },
-        {
-        id: 2,
-        ProjectName: "Fichta",
-        ProjectTechUsed: [
-            "Next Js",
-            "styled-Components",
-            "Redux toolkit",
-            "vercel",
-            "ethers js",
-            "MetaMask"
-        ],
-        ProjectDescription: "A web3 Ecommerce dapp for grocery, connected to ethers and blockchain. all payment is done in crypto currency. ",
-            ProjectImage: Project5,
-          ProjectUrl: "https://fichta.vercel.app/",
-        order: 1,
-    },
-    {
-        id: 3,
-        ProjectName: "NextFood Template",
-        ProjectTechUsed: [
-            "React Js",
-            "Next js",
-            "Styled-Components",
-            "Vercel"
-        ],
-        ProjectDescription: "A web app for Food Website template.  this project includes all the features of the website.",
-        ProjectImage: Project,
-        ProjectUrl: "https://nextfood.vercel.app",
-        order: 0,
-    },
-    {
-        id: 4,
-        ProjectName: "Ishortner",
-        ProjectTechUsed: [
-            "React Js",
-            "styled-Components",
-            "Netlify"
-        ],
-        ProjectDescription: "A web app for url link shortner to give easy and memorable url to your website.",
-        ProjectImage: Project1,
-          ProjectUrl: "https://ishortner.netlify.app/",
-        order: 1,
-    },
-    {
-        id: 5,
-        ProjectName: "Wpost ",
-        ProjectTechUsed: [
-            "React Js",
-            "styled-Components",
-            "Netlify"
-        ],
-        ProjectDescription: "A fun Practice template which i built during my learning phrase. ",
-        ProjectImage: Project2,
-          ProjectUrl: "https://wpost.netlify.app/",
-        order: 0,
-    },
-        {
-        id: 6,
-        ProjectName: "Xdome Todo Application ",
-        ProjectTechUsed: [
-            "Next Js",
-            "styled-Components",
-            "Redux toolkit",
-            "vercel",
-        ],
-        ProjectDescription: "A TODO web app managed with redux toolkit, slick design and well structured components.",
-            ProjectImage: Project3,
-          ProjectUrl: "https://xdome.vercel.app/",
-        order: 1,
-    },
+  {
+    id: 10,
+    ProjectName: "Drop-hq  ",
+    ProjectTechUsed: ["Next Js", "SCSS", "Redux ", "Vercel", "Node js"],
+    ProjectDescription: "A Food Ordering and delivery service.",
+    ProjectImage: Drophq,
+    ProjectUrl: "https://drop-hq.vercel.app/",
+    order: 0,
+  },
+  {
+    id: 1,
+    ProjectName: "!Vitriol  ",
+    ProjectTechUsed: [
+      "Next Js",
+      "styled-Components",
+      "vercel",
+      "ethers js",
+      "MetaMask",
+    ],
+    ProjectDescription:
+      "A web3 charity dapp Website template. Awesome UI , connected to ethers and blockchain.",
+    ProjectImage: Project4,
+    ProjectUrl: "https://vitriol-neon.vercel.app/",
+    order: 1,
+  },
+  {
+    id: 5,
+    ProjectName: "Credizest ",
+    ProjectTechUsed: ["Next Js", "styled-Components", "Vercel"],
+    ProjectDescription: "Credizest is a Fintech landing page template.",
+    ProjectImage: Credizest,
+    ProjectUrl: "https://credizestx.vercel.app/",
+    order: 0,
+  },
+  {
+    id: 2,
+    ProjectName: "Fichta",
+    ProjectTechUsed: [
+      "Next Js",
+      "styled-Components",
+      "Redux toolkit",
+      "vercel",
+      "MetaMask",
+    ],
+    ProjectDescription:
+      "A web3 Ecommerce dapp for grocery, connected to ethers and blockchain. all payment is done in crypto currency. ",
+    ProjectImage: Project5,
+    ProjectUrl: "https://fichta-mart.vercel.app/",
+    order: 1,
+  },
+  {
+    id: 3,
+    ProjectName: "NextFood Template",
+    ProjectTechUsed: ["React Js", "Next js", "Styled-Components", "Vercel"],
+    ProjectDescription:
+      "A web app for Food Website template.  this project includes all the features of the website.",
+    ProjectImage: Project,
+    ProjectUrl: "https://next-food-app-nine.vercel.app/",
+    order: 0,
+  },
+  {
+    id: 4,
+    ProjectName: "Ishortner",
+    ProjectTechUsed: ["React Js", "styled-Components", "Netlify"],
+    ProjectDescription:
+      "A web app for url link shortner to give easy and memorable url to your website.",
+    ProjectImage: Project1,
+    ProjectUrl: "https://ishortner.netlify.app/",
+    order: 1,
+  },
 
-]
+  {
+    id: 6,
+    ProjectName: "Xdome Todo Application ",
+    ProjectTechUsed: [
+      "Next Js",
+      "styled-Components",
+      "Redux toolkit",
+      "vercel",
+    ],
+    ProjectDescription:
+      "A TODO web app managed with redux toolkit, slick design and well structured components.",
+    ProjectImage: Project3,
+    ProjectUrl: "https://xdome-todo.vercel.app/",
+    order: 1,
+  },
+];
 
-export default AllProjectData
+export default AllProjectData;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,29 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import Navigation from "../components/Navigation"
-import HeroIntro from "../components/HeroIntro"
-import Services from '../components/Services'
-import FloatComponents from '../components/FloatComponents'
-import Projects from '../components/Projects'
-import Contact from '../components/Contact'
+import Navigation from "../components/Navigation";
+import HeroIntro from "../components/HeroIntro";
+import Services from "../components/Services";
+import FloatComponents from "../components/FloatComponents";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Blyncnov - Frontend Developer Portfolio" />
+        <title>Blyncnov | Front-End Engineer</title>
+        <meta
+          name="description"
+          content="Blyncnov - Frontend Developer Portfolio"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navigation />
       <HeroIntro />
-      <Services/>
-      <FloatComponents  value="bholuwatife00@gmail.com"/>
-      <FloatComponents left="1em" right="0" value="https://github.com/blyncnov" />
+      <Services />
+
       <Projects />
-      <Contact/>
+      <Contact />
     </>
-  )
+  );
 }
